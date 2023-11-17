4fb7243d0a1d32eb4c948bfe976f7d42f7e8e2ba
AnaMaria9105 <149152000+AnaMaria9105@users.noreply.github.com>
2023-11-17 15:33:36 +0200

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ app.set('port', (process.env.PORT || 80 ))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send('Hello Horia!')
+  response.send('Hello Horiaa!')
 })
 
 app.listen(app.get('port'), function() {
